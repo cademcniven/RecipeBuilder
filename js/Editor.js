c90d1ef02bfb513608f9a9ffd9a7dba395e64bb5
cademcniven <cademcniven@gmail.com>
2019-12-17 11:22:42 -0800

    can add qty and unit for ingredients

--- js/Editor.js
+++ js/Editor.js
@@ -3,10 +3,13 @@ const applicationID = '2b00e4f3';
 const applicationKey = '1c03d18e74f85d034dd5b0cb91fb2473';
 
 const getUrl = (query) => `https://api.edamam.com/api/food-database/parser?ingr=${query}&app_id=${applicationID}&app_key=${applicationKey}`;
+const nutritionUrl = "https://api.edamam.com/api/food-database/nutrients";
 const isAlphabet = (str) => /^[a-zA-Z ]+$/.test(str)
 
+var ingredientSearch;
+
 window.onload = (event) => {
-    const ingredientSearch = document.getElementById('ingredientSearch');
+    ingredientSearch = document.getElementById('ingredientSearch');
     const ingredientSearchButton = document.getElementById('ingredientSearchButton');
 
     ingredientSearchButton.addEventListener("click", SearchIngredient);
@@ -19,7 +22,6 @@ window.onload = (event) => {
         }
     }
 };
-
 
 function SearchIngredient(event) {
     var ingredient = ingredientSearch.value;
@@ -51,7 +53,7 @@ function DisplayIngredientMenu(ingredients) {
             return;
 
         ingredientNames.add(ingredient.food.label.toLowerCase());
-        CreateModalHTML(ingredient.food.label.toLowerCase(), ingredient.food.foodId);
+        CreateModalHTML(ingredient.food.label.toLowerCase(), ingredient.food.foodId, JSON.stringify(ingredient.measures));
     });
 }
 
@@ -64,23 +66,47 @@ function CloseModal() {
     document.querySelectorAll("#ingredientOptions div").forEach(e => e.parentNode.removeChild(e));
 }
 
-function CreateModalHTML(label, foodId) {
+function CreateModalHTML(label, foodId, units) {
     var parent = document.getElementById("ingredientOptions");
     var newDiv = document.createElement('div');
-    var domString = `<button type="button" onclick="addIngredientToRecipe(this)" data-label="${label}" data-id="${foodId}" class="ingredientListButton">x</button> ${label}`;
+    var domString = `<button type="button" onclick="addIngredientToRecipe(this)" data-label="${label}" data-id="${foodId}" 
+                        data-units=${units} class="ingredientListButton">x</button> ${label}`;
     newDiv.innerHTML = domString;
     parent.appendChild(newDiv);
 }
 
 function addIngredientToRecipe(buttonClicked) {
-    CreateIngredientHTML(buttonClicked.dataset.label);
+    CreateIngredientHTML(buttonClicked.dataset.label, buttonClicked.dataset.units);
     CloseModal();
+
 }
 
-function CreateIngredientHTML(label) {
+function CreateIngredientHTML(label, units) {
     var parent = document.getElementById("ingredientList");
     var li = document.createElement('li');
-    var domString = `<li>${label}</li>`;
+    var unitOptions = CreateUnitHTML(units);
+    var domString = `<li>
+                    Qty
+                    <input type="text" class="qty" name="qty" value="0">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    Unit
+                    ${unitOptions}
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    ${label} 
+                    </li>`;
     li.innerHTML = domString;
     parent.appendChild(li);
+}
+
+function CreateUnitHTML(units) {
+    var unitObj = JSON.parse(units);
+    var select = `<select>`
+
+    unitObj.forEach(unit => {
+        select += `<option value="${unit.uri}">${unit.label}</option>`;
+    });
+
+    select += `</select>`
+
+    return select;
 }
