88b60af7eb65e0d8233bb6267faedd786205a42f
cademcniven <cademcniven@gmail.com>
2019-12-28 08:59:52 -0800

    can create accounts and log in

--- routes/user.js
+++ routes/user.js
@@ -11,3 +11,49 @@ router.get('/:id', async(req, res) => {
     const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
     res.send(rows[0])
 })
+
+router.post('/', async(req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    if (password == password2) {
+        try {
+            const { rows } = await db.query("INSERT INTO users (username, password) VALUES ($1,$2)", [username, password]);
+            request.session.loggedin = true;
+            request.session.username = username;
+        } catch (error) {
+            console.log("failed to create user");
+            res.redirect('/Register.html');
+        }
+
+        res.redirect('/Editor.html');
+    } else {
+        res.redirect('/Register.html');
+    }
+})
+
+router.post('/auth', async(request, response) => {
+    //console.log(request.session.username);
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        let rows;
+        try {
+            rows = await db.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
+        } catch (error) {
+            console.log(error.stack);
+        }
+
+        console.log(rows.rows[0]);
+        if (typeof rows.rows[0] != 'undefined') {
+            request.session.loggedin = true;
+            request.session.username = username;
+            response.redirect('/Editor.html');
+        } else {
+            response.redirect('/Login.html');
+        }
+
+        response.end();
+    }
+});
--- server.js
+++ server.js
@@ -3,8 +3,24 @@ var express = require('express'),
     port = process.env.PORT || 3000;
 
 var cors = require('cors');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
 app.use(cors());
+app.use(session({
+    secret: 'dfadskajfhsdklfjhsa543425345gdsfd',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var mountRoutes = require('./routes');
 mountRoutes(app);
+
+app.use(express.static('html'));
+app.use(express.static('css'));
+app.use(express.static('js'));
+
 app.listen(port);
